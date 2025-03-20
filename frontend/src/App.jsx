64f15618c96fffb30d7a097856fb3383f3ba8ced
Saori <kumakichi3173@gmail.com>
2025-03-19 23:08:15 -0700

    Created HomePage, CreatePage, and Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 
-import { Button } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
+import CreatePage from './pages/CreatePage'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
     <>
-
+      <Box minH={"100vh"}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+      </Box>
       <Button>hello</Button>
-
     </>
   )
 }
