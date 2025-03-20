972e33adc625b4ed709e19f9321c4cba0bf3dc31
Saori <kumakichi3173@gmail.com>
2025-03-19 22:44:37 -0700

    Add BrowserRouter in main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import { Button } from "@chakra-ui/react"
 
+import { Button } from '@chakra-ui/react'
 
 function App() {
 
   return (
     <>
+
+      <Button>hello</Button>
 
     </>
   )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
+import { Provider } from './components/ui/provider'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
+  <StrictMode>
+    <Provider>
+      <BrowserRouter>
       <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>,
 )
