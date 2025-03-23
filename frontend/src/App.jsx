33e15ca7549702fffa0d95ed1671894cdb0455ff
Saori <kumakichi3173@gmail.com>
2025-03-22 23:35:52 -0700

    Change the bg color of BOX

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,13 @@ import { Route, Routes } from 'react-router-dom'
 import CreatePage from './pages/CreatePage'
 import HomePage from './pages/HomePage'
 import Navbar from './components/ui/Navbar'
+import { useColorModeValue } from './components/ui/color-mode'
 
 function App() {
 
   return (
     <>
-      <Box minH={"100vh"}>
+      <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
