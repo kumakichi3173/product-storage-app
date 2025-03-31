b52d90c82220f19b1ad91ea205180d4cde8324a4
Saori <kumakichi3173@gmail.com>
2025-03-30 22:58:26 -0700

    Add Container in HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,21 @@
+import { Container, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      
-    </div>
+    <Container maxW='container .xl' py={12}>
+      <VStack spacing={8}>
+      <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 💍
+        </Text>
+      </VStack>
+    </Container>
   )
 }
 
