ed48bd396cba34b9f27e5861eb534a07652be63b
Saori <kumakichi3173@gmail.com>
2025-04-09 10:04:51 +0900

    Add texits in HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Container, Text, VStack } from '@chakra-ui/react'
+import { Container, Link, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const HomePage = () => {
@@ -6,13 +6,22 @@ const HomePage = () => {
     <Container maxW='container .xl' py={12}>
       <VStack spacing={8}>
       <Text
-          fontSize={"30"}
+            fontSize='3xl'
           fontWeight={"bold"}
-          bgGradient={"linear(to-r, cyan.400, blue.500)"}
-          bgClip={"text"}
+          // bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          // bgClip={"text"}
           textAlign={"center"}
         >
           Current Products 💍
+        </Text>
+
+        <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+        Prodct not found {" "}   
+        <Link to ={"/create"}>
+        <Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
+          Create a product
+        </Text>
+        </Link>       
         </Text>
       </VStack>
     </Container>
